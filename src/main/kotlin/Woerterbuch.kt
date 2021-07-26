6a024f33df70447060b1c0e07e097470396409be
Christiane Zapp <46904550+BuggySama@users.noreply.github.com>
2021-07-26 19:06:30 +0200

    core functionalities implemented.

--- src/main/kotlin/Woerterbuch.kt
+++ src/main/kotlin/Woerterbuch.kt
@@ -1,7 +1,7 @@
 import kotlin.collections.arrayListOf
 class Woerterbuch
 {
-    var woerter:ArrayList<String> =arrayListOf("Garten", "neuer", "Akasha", "neue", "Pflanze", "einsetzen")
+    var woerter:ArrayList<String> =arrayListOf("Garten", "neuer", "Akasha", "neue", "Pflanze", "einsetzen", "ernten", "waessern", "duengen", "Chakrastein", "hinzufuegen", "entfernen", "pruefen")
     //supports:
     // neuer Garten axb
     // neue Akasha auf a
