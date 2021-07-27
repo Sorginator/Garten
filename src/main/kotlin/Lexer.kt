72b38afa73fe7f696bceacbd55ee278fa2008411
Sorginator <62674320+Sorginator@users.noreply.github.com>
2021-07-27 16:32:22 +0200

    Update Lexer.kt

--- src/main/kotlin/Lexer.kt
+++ src/main/kotlin/Lexer.kt
@@ -6,15 +6,16 @@ fun readCode(inputString: String) {
 
 fun getExpression(potentialExpression: String) {
     when(potentialExpression) {
-        "createGarden" -> TODO()
-        "expandGarden" -> TODO()
-        "decreaseGarden" -> TODO()
-        "plan" -> TODO()
-        "harvest" -> TODO()
-        "water" -> TODO()
-        "trimm" -> TODO()
-        "fertilize" -> TODO()
-        "commute" -> TODO()
+        "gartenErstellen" -> TODO()
+        "gartenErweitern" -> TODO()
+        "gartenVerkleinern" -> TODO()
+        "pflanzen" -> TODO()
+        "ernten" -> TODO()
+        "bewässern" -> TODO()
+        "beschneiden" -> TODO()
+        "düngen" -> TODO()
+        "auspendeln" -> TODO()
+        "var" -> TODO()
         else -> null
     }
 }
